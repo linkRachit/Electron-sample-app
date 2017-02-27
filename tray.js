33ee9cb411dc0a968dbf1420d8776867e69d2c34
Rachit Goel <rachit2goel@gmail.com>
2017-02-27 21:44:13 +0530

    tray completed

---
+++ tray.js
@@ -0,0 +1,61 @@
+'use strict'
+const path = require('path')
+const electron = require('electron')
+
+const app = electron.app
+const shell = electron.shell
+const Tray = electron.Tray
+const Menu = electron.Menu
+
+const trayIconDefault = path.join(__dirname, 'static/icon-18x18.png')
+const trayIconWindows = path.join(__dirname, 'static/icon.ico')
+let tray = null
+
+exports.create = win => {
+  if (process.platform === 'darwin' || tray) {
+    return
+  }
+
+  let icon = trayIconDefault
+
+  if (process.platform === 'win32') {
+    icon = trayIconWindows
+  }
+
+  const toggleWin = () => {
+    if (process.platform === 'win32') {
+      if (win.isMinimized()) {
+        win.restore()
+      } else if (win.isVisible()) {
+        win.hide()
+      } else {
+        win.show()
+      }
+    } else {
+      win.isVisible() ? win.hide() : win.show()
+    }
+  }
+
+  // Create toolbar
+  tray = new Tray(icon)
+
+  const contextMenu = [{
+    label: 'Toggle',
+    click() {
+      toggleWin()
+    }
+  }, {
+    type: 'separator'
+  }, {
+    label: 'Quit',
+    click() {
+      app.quit()
+    }
+  }]
+
+  tray.setToolTip(`${app.getName()}`)
+  tray.setContextMenu(Menu.buildFromTemplate(contextMenu))
+  tray.on('click', function handleClicked() {
+    toggleWin()
+  })
+}
